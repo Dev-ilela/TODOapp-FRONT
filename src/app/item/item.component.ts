520352c2af039279a810862fa402acb197832af1
Gabriel Vilela <gabrielvilelacarvalho@gmail.com>
2025-04-25 23:19:49 -0300

    front atualizado

---
+++ src/app/item/item.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Tarefa } from "../tarefa";
+
+@Component({
+  selector: 'app-item',
+  standalone: false,
+  templateUrl: './item.component.html',
+  styleUrl: './item.component.css'
+})
+
+export class ItemComponent {
+  emEdicao = false;
+  @Input() tarefa: Tarefa = new Tarefa("", false);
+  @Output() remove = new EventEmitter();
+  @Output() modificaTarefa = new EventEmitter();
+
+  onRemover() {
+    this.remove.emit();
+  }
+}
